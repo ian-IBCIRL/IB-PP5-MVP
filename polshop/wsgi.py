0af0cdc63fb870035e8d6514835242ab0edfdf99
ian-IBCIRL <54858597+ian-IBCIRL@users.noreply.github.com>
2023-04-29 19:14:45 +0100

    amended DJANGO_SETTTINGS_MODULE

--- polshop/wsgi.py
+++ polshop/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pp5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polshop.settings')
 
 application = get_wsgi_application()
