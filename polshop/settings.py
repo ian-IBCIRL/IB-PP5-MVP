f02ab0c56da33e813149fe8dd8179e576ecc05d6
ian-IBCIRL <ian@ibcirl.com>
2023-05-01 13:14:31 +0000

    added base.css and static files

--- polshop/settings.py
+++ polshop/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ib-pp5-polshop.herokuapp.com', 'localhost']
 
@@ -181,7 +181,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]  # noqa static location for favicon
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # noqa static location for favicon
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
