ee7bebcb0cbe7e81c52f44ce765704dd35044280
ian-IBCIRL <ian@ibcirl.com>
2023-05-06 17:52:01 +0000

    updated search and allauth settings

--- polshop/urls.py
+++ polshop/urls.py
@@ -21,7 +21,7 @@ from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('privacy/', include('privacy.urls')),
