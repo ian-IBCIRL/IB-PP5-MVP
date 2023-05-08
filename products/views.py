773e37c0482081e5a858fb91b9f532ac249e663c
ian-IBCIRL <ian@ibcirl.com>
2023-05-08 20:14:04 +0000

    updated product management httpresponse

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse
+from django.shortcuts import HttpResponse,  get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
