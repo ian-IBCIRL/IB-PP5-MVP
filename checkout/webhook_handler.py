1c66db7fd299505e682d0459f81c9eb653396151
ian-IBCIRL <ian@ibcirl.com>
2023-05-17 18:45:41 +0000

    updated webhook code for ordersheet

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -123,7 +123,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    user_profile=profile,                    
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
