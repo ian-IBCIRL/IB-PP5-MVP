c63504d859fb1be3831cbc5bc6453451ffccfeea
ian-IBCIRL <ian@ibcirl.com>
2023-05-17 14:40:03 +0000

    updated site settings and views

--- polshop/views.py
+++ polshop/views.py
@@ -6,6 +6,6 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
-def handler500(request, exception=None):
+def handler500(request, exception):
     """ Error Handler 500 Internal Server error"""
     return render(request, "errors/500.html", status=500)
