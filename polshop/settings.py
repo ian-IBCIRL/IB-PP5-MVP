1f4352ed2bf871d23e2d660fd82eb1647ab7d04e
ian-IBCIRL <ian@ibcirl.com>
2023-05-18 20:33:28 +0000

    adding checkout confirmation_emails to templates dir in settings.py

--- polshop/settings.py
+++ polshop/settings.py
@@ -103,6 +103,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
+            os.path.join(BASE_DIR, 'checkout', 'confirmation_emails'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
