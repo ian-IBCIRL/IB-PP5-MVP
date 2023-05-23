908a9d15793462b3110f8b0b2552022ae816826c
ian-IBCIRL <ian@ibcirl.com>
2023-05-23 09:30:59 +0000

    fixed 500 handler

--- polshop/urls.py
+++ polshop/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +31,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'polshop.views.handler404'
+handler500 = 'polshop.views.handler500'
--- polshop/views.py
+++ polshop/views.py
@@ -6,6 +6,6 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
-def handler500(request, exception):
+def handler500(request):
     """ Error Handler 500 Internal Server error"""
     return render(request, "errors/500.html", status=500)
