015519ebdc3cb56a4b0bc8af065648e380a96b06
ian-IBCIRL <ian@ibcirl.com>
2023-05-26 13:51:18 +0000

    updated static

--- polshop/settings.py
+++ polshop/settings.py
@@ -218,7 +218,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # noqa static location for favicon
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
