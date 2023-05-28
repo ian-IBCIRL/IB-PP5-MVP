ba756a8155401452447a2da63ba289b1b6e867a8
ian-IBCIRL <ian@ibcirl.com>
2023-05-28 14:51:07 +0000

    updated automated testing and production settings

--- polshop/settings.py
+++ polshop/settings.py
@@ -138,24 +138,24 @@ SITE_ID = 1
 
 WSGI_APPLICATION = 'polshop.wsgi.application'
 
-# Use the following database details for testing
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DEVELOPMENT = True
+DEVELOPMENT = False
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'polshop@example.com'
 
 else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+# Use the following database details for automated testing
 #    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': BASE_DIR / 'db.sqlite3',
+#        }
 #    }
+
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
