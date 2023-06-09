d4df7ab9145984d3a9d7eb2474aad70fdc9f2b59
ian-IBCIRL <54858597+ian-IBCIRL@users.noreply.github.com>
2023-06-09 15:36:06 +0100

    Update models.py for comments and possible size differences

--- checkout/models.py
+++ checkout/models.py
@@ -11,6 +11,9 @@ from profiles.models import UserProfile
 
 
 class Order(models.Model):
+    """
+    The model to keep track of orders in processing, allowing for bag name change to ordersheet.
+    """    
     order_number = models.CharField(max_length=32, null=False, editable=False)
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='orders')  # noqa
@@ -72,12 +75,17 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
+    """
+    A model for each product that is part of each order.
+    """    
     order = models.ForeignKey(Order, null=False, blank=False,
                               on_delete=models.CASCADE,
                               related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False,
                                 on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True)  # noqa XS, S, M, L, XL
+    product_size = models.CharField(max_length=2, null=True, blank=True)  
+    # noqa XS, S, M, L, XL - considering changing to hours, days, weeks etc
+    
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6,
                                          decimal_places=2,
