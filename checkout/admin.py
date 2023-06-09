e2b51da4c8cceb43613c81a89527c3150eb4c2f5
ian-IBCIRL <54858597+ian-IBCIRL@users.noreply.github.com>
2023-06-09 15:28:05 +0100

    Update admin.py # comments adjusted for Stripe ordering and bag object name change to ordersheet

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,18 +3,25 @@ from .models import Order, OrderLineItem
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Order line items display section for admin
+    """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
+    """
+    Order management section for admin
+    """
     inlines = (OrderLineItemAdminInline,)
 
+    # adjusted for Stripe ordering and bag object name change to ordersheet
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
                        'grand_total', 'original_ordersheet',
                        'stripe_pid')
-
+    
     fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
