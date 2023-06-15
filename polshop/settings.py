a4b39c51ae7632d60f1d1d7c250ddbf7ec2e3487
ian-IBCIRL <ian@ibcirl.com>
2023-06-15 08:04:43 +0000

    adjusted debug to false

--- polshop/settings.py
+++ polshop/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ib-pp5-polshop.herokuapp.com', '8000-ianibcirl-ibpp5mvp-zetl4nvtfpd.ws-eu99.gitpod.io', 'localhost']  # noqa
 
